238e1fab9378197aa3cfdcb5932124264f072528
Tsuru <129417634+tsurubr@users.noreply.github.com>
2024-03-24 17:35:49 -0300

    Add files via upload

---
+++ basic_data_structures/lancadados.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define vl vector<long long>
+#define vi vector<int>
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0)
+#define endl '\n'
+#define pb push_back
+
+void solve(){
+  int n; cin >> n;
+  map<int, int> cnt;
+  vector<int> v(n);
+
+  for(auto &x : v) cin >> x;
+  for(auto &y : v) {
+    cnt[y]++;
+  }
+  int maxocrr = 0;
+  for(auto &par: cnt) {
+    if(par.second > maxocrr) {
+      maxocrr = par.second;
+    }
+  }
+  for(const auto x: cnt) {
+    if (x.second == maxocrr) cout << x.first << " ";
+  }
+  cout << endl;
+}
+
+int main(){
+  fast_io; 
+  //int t; cin>>t; while(t--)
+  solve();
+
+  return 0;
+}
+
